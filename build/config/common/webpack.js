5759ab03602ede19d361d83ce7c16b085b75c971
Seba Gamboa <me@sagmor.com>
2015-11-12 13:16:42 -0300

    Sorting files around

---
+++ build/config/common/webpack.js
@@ -0,0 +1,40 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ROOT = __dirname + '/../../..';
+
+module.exports = {
+  context: ROOT,
+  entry: "./app/config/boot.jsx",
+  output: {
+    filename: "bundle-[hash].js",
+    path: ROOT + "/build",
+  },
+  devtool: 'cheap-module-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: "babel",
+        query:
+        {
+          presets:['es2015','react']
+        }
+      },
+      {
+        test: /\.html$/,
+        loader: "file",
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    root: [ROOT]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/config/index.html',
+      inject: 'body'
+    })
+  ]
+
+}
